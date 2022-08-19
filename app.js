d2ae6d0d2e94ef98b134fade6e56db76938c9c60
iKhizarSyed <khizarsyed786@gmail.com>
2022-08-19 20:04:54 +0530

    finally completed entire module on blogpost

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyparser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent = "THIS IS SOME CONTENT MADE FOR THE HOME PAGE";
 const aboutContent = "THIS IS SOME CONTENT MADE FOR THE ABOUT PAGE";
@@ -11,8 +12,46 @@ app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 app.use(bodyparser.urlencoded({ extended: true }));
 
+const posts = [];
+
 app.get("/", (req, res) => {
-  res.render("home", { startingcontent: homeStartingContent });
+  res.render("home", { startingcontent: homeStartingContent, posts: posts });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { aboutcontentnote: aboutContent });
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact", { ContactContentNote: contactContent });
+});
+
+app.get("/compose", (req, res) => {
+  res.render("compose");
+});
+
+app.post("/compose", (req, res) => {
+  const post = {
+    title: req.body.inputTitle,
+    content: req.body.postBody,
+  };
+  posts.push(post);
+  res.redirect("/");
+});
+
+app.get("/posts/:postName", (req, res) => {
+  const requestedTitle = _.lowerCase(req.params.postName);
+
+  posts.forEach((post) => {
+    const storedTitle = _.lowerCase(post.title);
+
+    if (storedTitle === requestedTitle) {
+      res.render("post", {
+        title: post.title,
+        content: post.content,
+      });
+    }
+  });
 });
 
 app.listen(3000, (req, res) => {
